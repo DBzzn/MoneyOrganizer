909b21d7e84847a14cdec0fd1e6af985b58f4360
Diogo <diogobzb17@outlook.com>
2026-03-12 13:01:54 -0300

    Categories setup OK! next step, transactions and Q/A

--- money-organizer-api/src/categories/categories.controller.ts
+++ money-organizer-api/src/categories/categories.controller.ts
@@ -1,6 +1,55 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Request,
+    UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
+interface AuthenticatedRequest extends Request {
+    user: { id: string, email: string };
+}
+
+@UseGuards(AuthGuard('jwt'))
 @Controller('categories')
 export class CategoriesController {
+    constructor(private readonly categoriesService: CategoriesService) { }
 
+    @Post()
+    create(
+        @Request() req: AuthenticatedRequest,
+        @Body() createCategoryDto: CreateCategoryDto
+    ) {
+        return this.categoriesService.create(req.user.id, createCategoryDto);
+    }
+
+    @Get()
+    findAll(@Request() req: AuthenticatedRequest) {
+        return this.categoriesService.findAll(req.user.id);
+    }
+
+    @Patch(':id')
+    update(
+        @Request() req: AuthenticatedRequest,
+        @Param('id') categoryId: string,
+        @Body() dto: UpdateCategoryDto
+    ) {
+        return this.categoriesService.update(req.user.id, categoryId, dto);
+    }
+
+    @Delete(':id')
+    remove(
+        @Request() req: AuthenticatedRequest,
+        @Param('id') categoryId: string
+    ) {
+        return this.categoriesService.remove(req.user.id, categoryId);
+    }
 }
